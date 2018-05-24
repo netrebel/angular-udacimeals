c6fcbf63750e7f9da53949f79236b61adf83040a
Miguel Reyes <miguel.reyes@enernoc.com>
2018-05-24 15:35:05 -0400

    Directives:
    ng-repeat, ng-model, ng-if, ng-click

--- app/scripts/controllers/brick.js
+++ app/scripts/controllers/brick.js
@@ -9,8 +9,37 @@
  */
 angular.module('udaciMealsApp')
   .controller('BrickCtrl', function () {
-    this.color = 'Green';
-    this.size = 'Size: 2 x 6';
-    this.price = '$0.03';
-    this.quantity = 697;
+    this.items = [
+      {
+        color : 'Green',
+        size : 'Size: 2 x 6',
+        price : '$0.03',
+        quantity : 697,
+        rating : 5
+      },
+      {
+        color : 'Red',
+        size : 'Size: 2 x 6',
+        price : '$0.03',
+        quantity : 10,
+        rating : 2
+      },
+      {
+        color : 'Yellow',
+        size : 'Size: 2 x 6',
+        price : '$0.03',
+        quantity : 300,
+        rating : 1
+      }
+    ];
+
+    this.increment = function(item) {
+      item.rating = ((item.rating * 10) + 1)/ 10;
+    };
+    this.decrement = function(item) {
+      item.rating = ((item.rating * 10) - 1)/ 10;
+    };
+
+
+
   });
