76f4f1249a984217ece5b4c3ebb7830ffa6daaa9
Miguel Reyes <miguel.reyes@enernoc.com>
2018-05-24 13:09:46 -0400

    yo angular:controller brick

---
+++ app/scripts/controllers/brick.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name udaciMealsApp.controller:BrickCtrl
+ * @description
+ * # BrickCtrl
+ * Controller of the udaciMealsApp
+ */
+angular.module('udaciMealsApp')
+  .controller('BrickCtrl', function () {
+    this.color = 'Green';
+    this.size = 'Size: 2 x 6';
+    this.price = '$0.03';
+    this.quantity = 697;
+  });
